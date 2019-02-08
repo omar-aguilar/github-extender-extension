62201c7cd7029c0ef12540aee66aef61997373ad
Omar Aguilar <omar2687a@gmail.com>
2019-02-08 10:17:26 -0600

    get all data for each request

--- src/background/prUtils.js
+++ src/background/prUtils.js
@@ -1,7 +1,40 @@
-const _getData = (repo, owner, path, token) => {
-  const basePath = 'https://api.github.com/repos/';
-  const url = `${basePath}${repo}/${owner}/${path.join('/')}`;
-  return fetch(url, {
+const _getObjFromQueryParams = (stringUrl) => {
+  const { search: urlSearch } = new URL(stringUrl.replace(/<|>/g, ''));
+  return urlSearch.replace('?', '')
+    .split('&')
+    .map(queryParam => queryParam.split('='))
+    .reduce((result, [key, value]) => ({
+      ...result,
+      [key]: value,
+    }), {});
+};
+
+const _getQueryParamsFromObj = (params) => {
+  if (!params || typeof params !== 'object') {
+    return '';
+  }
+  const queryParams = Object.keys(params)
+    .map(key => `${key}=${encodeURIComponent(params[key])}`)
+    .join('&');
+  return queryParams ? `?${queryParams}` : '';
+};
+
+const _getLastPageFromLink = (link) => {
+  let lastPage = 1; // when there are no links in headers
+  if (link) {
+    const lastLink = link.split(',').find(ref => /rel=.*last.*/.test(ref));
+    if (lastLink) {
+      const [linkUrl] = lastLink.split(';');
+      const queryParams = _getObjFromQueryParams(linkUrl);
+      lastPage = +queryParams.page || lastPage;
+    }
+  }
+  return lastPage;
+};
+
+const _GET = (url, token, queryParams) => {
+  const urlWitParams = `${url}${_getQueryParamsFromObj(queryParams)}`;
+  return fetch(urlWitParams, {
     headers: {
       Authorization: `token ${token}`,
     },
@@ -11,6 +44,34 @@ const _getData = (repo, owner, path, token) => {
       console.log('error in get request, url', url, error);
       Promise.resolve(null);
     });
+};
+
+const _getData = (repo, owner, path, token) => {
+  const basePath = 'https://api.github.com/repos/';
+  const url = `${basePath}${repo}/${owner}/${path.join('/')}`;
+  return _GET(url, token);
+};
+
+const _getAllData = (repo, owner, path, token) => {
+  const basePath = 'https://api.github.com/repos/';
+  const url = `${basePath}${repo}/${owner}/${path.join('/')}`;
+  return fetch(url, {
+    method: 'HEAD',
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  })
+    .then((response) => {
+      const link = response.headers.get('Link');
+      return _getLastPageFromLink(link);
+    })
+    .then((lastPage) => {
+      const promises = [...Array(lastPage)].map((_, idx) => (
+        _GET(url, token, { page: idx + 1 })
+      ));
+      return Promise.all(promises);
+    })
+    .then(responses => responses.reduce((result, next) => result.concat(next)));
 };
 
 const getOpenPRsInfo = (owner, repo, token) => _getData(owner, repo, ['pulls'], token)
@@ -30,7 +91,7 @@ const getOpenPRsInfo = (owner, repo, token) => _getData(owner, repo, ['pulls'], 
         ['pulls', number, 'reviews'],
         ['issues', number, 'comments'],
       ];
-      const metaPromises = paths.map(path => _getData(owner, repo, path, token));
+      const metaPromises = paths.map(path => _getAllData(owner, repo, path, token));
       return Promise.all([pr, lastCommit, ...metaPromises]);
     }),
   ))
